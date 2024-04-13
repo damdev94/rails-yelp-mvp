06fd78232e599cd0b9e0d5316a41a1840f4ed550
damien viel <damienviel69@gmail.com>
2024-04-13 12:52:47 -0400

    Yelp MVP

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,13 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resources :restaurant, only: [:index, :show, :create, :new] do
+    resources :review, only:[:new, :create]
+    member do
+      get :reviews
+    end
+  end
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: 'japanese', phone_number: '0664001059'}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: 'italian', phone_number: '0687954878'}
+tokyo_store = {name: "Tokyo Store", address: "1400 pins avenue, Montreal", category: 'japanese', phone_number: '0654789632'}
+chez_loic = {name: "Chez Loic", address: "9678 garnier street, Montreal", category: "french", phone_number: '0685412369'}
+rosy = {name: "Rosy", address: "5878 sainte catherine, Montreal", category: "chinese", phone_number: '0645782195'}
+
+[dishoom, pizza_east, tokyo_store, chez_loic, rosy].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
