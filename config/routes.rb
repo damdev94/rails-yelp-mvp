e3568c0f61c53b535d411b378861c8d181764894
damien viel <damienviel69@gmail.com>
2024-04-14 15:04:10 -0400

    Yelp MVP

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :restaurant, only: [:index, :show, :create, :new] do
-    resources :review, only:[:new, :create]
+  resources :restaurants, only: [:index, :show, :create, :new] do
+    resources :reviews, only:[:create]
     member do
       get :reviews
     end
